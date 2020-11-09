8b1cb26e450ac04426d2624c89bd97c5ac2c4bec
mysta3 <myshawnestallings@gmail.com>
2020-11-08 18:52:26 -0800

    added OR and AND conditional

--- conditionals.rb
+++ conditionals.rb
@@ -14,3 +14,30 @@
 # elsif  5 > 7
 #     puts " Oh ok"
 # end
+
+
+# grade = 'C'
+
+# if grade == 'A'
+#     puts ' Congrats'
+# elsif grade == 'B'
+#     puts 'Ya made it'
+# else 
+#     puts "dang"
+# end
+
+# AND conditional
+# age = 18
+# ticket = 'General'
+
+# if age < 21 && ticket == 'General'
+#     puts "Your id is now 1234"
+# end
+
+# OR conditional 
+# age = 22
+# ticket = 'Gen'
+
+# if age < 21 || ticket == 'Gen'
+#     puts 'You are not allowed to drink'
+# end
