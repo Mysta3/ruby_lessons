e7b809a248cffd7a68842a42bb624124aee5aaa5
mysta3 <myshawnestallings@gmail.com>
2020-11-19 16:36:55 -0800

    added some methods to string file

--- strings.rb
+++ strings.rb
@@ -119,3 +119,102 @@
 
 #bang is the exclamation point
 ##not available on every method, just some.
+
+# SPLIT METHOD
+# -> returns an array based on the delimiter
+# -> default delimiter is a space
+# -> change delimiter by providng an argument
+
+# sentence = "Hi, my name is Test. I have space in my budget"
+
+# p sentence.split
+# p sentence.split(".") # -> use a . as a delimiter 
+# p sentence.split("m")
+
+# def longest_word(sentence)
+#     # Write your code here
+#     arr = sentence.split(" ")
+#   arr.sort! { |a, b| b.length <=> a.length }
+#   p arr[0]
+# end
+
+# longest_word("Bobby loves big scary kangaroos")
+
+# ITERATE OVER STRING
+## break string into array using split or chars method
+
+# "Hello World".each_char{|char| puts char}
+# name = "test"
+# p name.split("") # -> returns array of characters
+# p name.chars # -> returns array of characters
+# letters = name.chars
+# letters.each { |letter| puts "#{letter} is awesome" }
+
+# p letters.join
+# -> passing an argument it will add a dash between the characters.
+
+# COUNT METHOD
+## -> counts like a humans mind
+## -> pass in argument to count # of occurrences
+# char = "Hello"
+# p char.count("l") # -> 2
+# p char.count("oHl") # -> 4 >> 1: H , 2: l, 1: o
+
+# def custom_count(string, search_characters)
+#     count = 0
+#     string.each_char do |char|
+#         if search_characters.include?(char)
+#             count += 1
+#         end
+#     end
+#     p count
+# end
+
+# custom_count("this is wild crazy", "il")
+
+# INDEX and RINDEX METHOD
+## index method -> returns index of character(s)
+## -> returns where the character first occurs
+## returns nil  if character is not found
+## rindex -> stands for reverse index or right index. 
+## does the same thing but in reverse
+# fact = "i am lit"
+
+# p fact.index("i") # -> returns 0 index
+# #pass in a second argument to start from that index
+# p fact.index("i", 3) # -> returns 6 index
+# fact = "I am handsome"
+# def custom_index(string, substring)
+#     return nil unless string.include?(substring)
+#     length = substring.length
+#     string.chars.each_with_index do |char, index|
+#         sequence = string[index, length]
+#         return index if sequence == substring
+#     end
+# end
+# p custom_index(fact, "I")
+
+# INSERT METHOD
+## mutates the original string
+# typo ="itchi"
+# typo.insert(2,"a") # (index where you want to place letter, letter to be inserted)
+# p typo
+
+# SEQUEEZE METHOD
+## removes any duplicate characters in a row, including spaces.
+## pass an argument to focus on a character to only remove that character
+# sentence = "Tthe aardvark jummped oooovvveerrrrr thhhhhe ffffeennnce"
+# # p sentence.squeeze  
+# def custom_squeeze(string)
+#     final = ""
+#     chars = string.split("")
+#     chars.each_with_index do |char, index|
+#         if char == chars[index + 1]
+#             next
+#         else
+#             final << char 
+#         end
+#     end
+#     final
+# end
+# p custom_squeeze(sentence)
