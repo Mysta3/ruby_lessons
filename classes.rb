797874616367f94673fa8d99e4038f57744ef547
mysta3 <myshawnestallings@gmail.com>
2020-12-03 17:01:51 -0800

    added classes file

---
+++ classes.rb
@@ -0,0 +1,12 @@
+# ATTRIBUTES AND METHODS
+## instance variables (attributes) belong to the object
+## Attributes provid einfo about the object's current statte
+## Instance methods belong to the object
+## Encapsulation restricts direct access to an object
+
+#SUPERCLASS & ANCESTORS METHODS
+## SUPERCLASS
+### class that a current class inherits from.
+### can be called on a class. Returns the parent class
+### subclass inherits form superclass 
+puts 5.methods.sort #-> returns methods in alphabetical order
